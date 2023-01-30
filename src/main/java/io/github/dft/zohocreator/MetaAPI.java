a125452bf1142b531e51bf880472961f882c9f82
Manan Thakkar <manan.thakkar@dakshyafreondtech.com>
2023-01-30 14:34:27 +0530

    Added pojos and method for zoho api in the sdk

--- src/main/java/io/github/dft/zohocreator/MetaAPI.java
+++ src/main/java/io/github/dft/zohocreator/MetaAPI.java
@@ -1,6 +1,5 @@
 package io.github.dft.zohocreator;
 
-import io.github.dft.zohocreator.constantcode.ConstantCodes;
 import io.github.dft.zohocreator.model.authenticationapi.AccessCredential;
 import io.github.dft.zohocreator.model.metaapi.ReportListRequest;
 import io.github.dft.zohocreator.model.metaapi.ReportWrapper;
@@ -10,8 +9,7 @@ import org.apache.http.client.utils.URIBuilder;
 
 import java.net.http.HttpRequest;
 
-import static io.github.dft.zohocreator.constantcode.ConstantCodes.API_BASE_END_POINT;
-import static io.github.dft.zohocreator.constantcode.ConstantCodes.SLASH_CHARACTER;
+import static io.github.dft.zohocreator.constantcode.ConstantCodes.*;
 
 public class MetaAPI extends ZohoCreatorSdk {
 
@@ -26,11 +24,11 @@ public class MetaAPI extends ZohoCreatorSdk {
             .concat(reportListRequest.getAccountOwnerName())
             .concat(SLASH_CHARACTER)
             .concat(reportListRequest.getAppLinkName())
-            .concat(ConstantCodes.REPORT_ENDPOINT));
+            .concat(REPORT_ENDPOINT));
 
         HttpRequest request = HttpRequest.newBuilder(uriBuilder.build())
-            .header(HttpHeaders.CONTENT_TYPE, ConstantCodes.CONTENT_TYPE_VALUE)
-            .headers(ConstantCodes.AUTHORIZATION_HEADER, ConstantCodes.TOKEN_NAME.concat(accessCredential.getAccessToken()))
+            .header(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE_VALUE)
+            .headers(AUTHORIZATION_HEADER, TOKEN_NAME.concat(accessCredential.getAccessToken()))
             .build();
 
         return getRequestWrapped(request, ReportWrapper.class);
